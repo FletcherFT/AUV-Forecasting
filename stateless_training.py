94a90d116e3c2ecfffab128de5f4670a1c838f8f
FletcherFT <fletcher.thompson@utas.edu.au>
2019-09-09 13:49:44 +0200

    First training on tx1

--- stateless_training.py
+++ stateless_training.py
@@ -3,8 +3,8 @@ import os
 from core.dataloader import StatelessDataLoader
 from core.model import DenseModel
 # force tensorflow to use CPU
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
-os.environ["CUDA_VISIBLE_DEVICES"] = ""
+#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
+#os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 
 # def main():
